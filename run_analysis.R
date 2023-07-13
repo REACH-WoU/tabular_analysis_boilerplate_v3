4528a2ad2ca5c84ab0877651149acf8e89ec8006
SvitlanaBozh <113593548+SvitlanaBozh@users.noreply.github.com>
2023-07-13 12:45:11 +0300

    fix JMMI_variable and retailers raion, oblast, reg

--- run_analysis.R
+++ run_analysis.R
@@ -7,12 +7,12 @@ JMMI_variable <- c("Customers")
 ## SET FILENAMES AND OTHER STRINGS  --------------------------------------------
 strings <- c(
   dataset.name = "[UKR] JMMI Ukraine",      # provide a full name for the title of output documents (e.g. "[POL] Post-Distribution Monitoring")
-  dataset.name.short = "JMMI UKR Customers Hromada",   # provide a short name for filenames of output documents (e.g. "POL_PDM")
+  dataset.name.short = paste0("JMMI UKR ", JMMI_variable, "Region"),   # provide a short name for filenames of output documents (e.g. "POL_PDM")
   dataset.date = "2023",       # this string is only used for creating titles for output documents
   out_date = stringr::str_sub(stringr::str_remove_all(Sys.Date(), '-'), 3),      # this one is appended to the end of filenames
   filename.data = "data/UKR2203JMMI_Customers_R15_clean_data_07JUN2023.xlsx",      # the filename of your data for analysis
-  filename.tool = "resources/Customers/UKR2203_JMMI_Questionnaire_Customers_R15_07JUN2023_SB.xlsx",      # filename of your kobo tool
-  filename.daf.tabular = "resources/Customers/DAF_hromada.xlsx"     # filename of your DAF
+  filename.tool = paste0("resources/", JMMI_variable, "/UKR2203_JMMI_Questionnaire_Customers_R15_07JUN2023_SB.xlsx"),      # filename of your kobo tool
+  filename.daf.tabular = paste0("resources/", JMMI_variable, "/DAF_region.xlsx")     # filename of your DAF
 )
 
 
@@ -28,14 +28,14 @@ params  <- c(
 source("src/init.R")
 
 ## Ananlysis of numeric variables  --------------------------------------------
-if (JMMI_variable == "Customers") {
+if (JMMI_variable == "Retailers") {
 source("JMMI_analysis_numeric.R")
 }
 
 ## TABULAR  -------------------------------------------------------------------
 
-rmarkdown::render('analysis_tabular_hromada.Rmd',
-                  output_file = paste0("output/Customers/", strings['dataset.name.short'], "_Tabular_Analysis_Hromada", strings['out_date'],".html"))
+rmarkdown::render('analysis_tabular_region.Rmd',
+                  output_file = paste0("output/", JMMI_variable, "/", strings['dataset.name.short'], "_Tabular_Analysis_Region", strings['out_date'],".html"))
 cat("\n> tabular analysis completed!")
 
 # ------------------------------------------------------------------------------
--- src/format_dataset_oblast.R
+++ src/format_dataset_oblast.R
@@ -2,6 +2,7 @@
 ### FORMAT DATASET
 ################################################################################
 
+if (JMMI_variable == "Customers") {
 # create shorthands to make working with the data easier:
 ## Weights
 # weights <- data.list$main %>%
@@ -35,5 +36,94 @@ we <- data.list$main %>%
 #   mutate(`b7_1_heating_fuel/none1` = pmax(`b7_1_heating_fuel/none_heating`,`b7_1_heating_fuel/none`,na.rm = TRUE)) %>%
 #   select(-c(`b7_1_heating_fuel/none_heating`,`b7_1_heating_fuel/none`)) %>%
 #   rename(`b7_1_heating_fuel/none` = `b7_1_heating_fuel/none1`)
+}
 
-                                         
+if (JMMI_variable == "Retailers") {
+  # create shorthands to make working with the data easier:
+  ## Weights
+  #weights <- data.list$main %>%
+  #   group_by(a6_current_raion) %>% 
+  #   summarise(n = n()) %>% 
+  #   rename(strata = "a6_current_raion") %>% 
+  #   left_join(sampling, by = "strata")%>% 
+  #   mutate(weight = (as.numeric(hh_total)/sum(as.numeric(hh_total)))/(n/sum(n))) %>% 
+  #   select(strata, weight) %>% 
+  #   rename(w = "strata")
+  ### ADD WEIGHTS, OVERALL AND GROUPING VARIABLES
+  #
+  data.list$main <- data.list$main %>% 
+    mutate(overall = "overall",
+           weight = 1 #as.numeric(weights)
+    ) %>%
+    relocate(`z1_food_price_increased/cereal_porridge`, .before = `z1_food_price_increased/all`) %>%
+    relocate(`v1_difficulties/storage_during_power_outages`, .before = `v1_difficulties/other`) %>%
+    relocate(`w3_access_stores/no_access_due_to_power_outages`, .before = `w3_access_stores/other`) %>%
+    relocate(`w3_access_stores/no_access_during_air_alert`, .before = `w3_access_stores/other`)
+  we <- data.list$main %>% 
+    select(weight, uuid) 
+  
+  ### Correcting z1 and z2 questions
+  
+  z1_cols <- c(
+    "z1_food_price_increased/bread",
+    "z1_food_price_increased/eggs",
+    "z1_food_price_increased/milk",
+    "z1_food_price_increased/potatoes",
+    "z1_food_price_increased/carrots",
+    "z1_food_price_increased/onions",
+    "z1_food_price_increased/cabbage",
+    "z1_food_price_increased/chicken",
+    "z1_food_price_increased/oil",
+    "z1_food_price_increased/flour",
+    "z1_food_price_increased/rice",
+    "z1_food_price_increased/buckwheat",
+    "z1_food_price_increased/water",
+    "z1_food_price_increased/cereal_porridge"
+  )
+  
+  pmax_food <- function(name_food){
+    return(pmax(name_food,data.list$main[["z1_food_price_increased/all"]]))
+  }
+  
+  data.list$main <- data.list$main %>%
+    mutate(across(.cols = z1_cols, .fns = pmax_food))
+  
+  z2_cols <- c(
+    "z2_nfi_price_increased/diapers",
+    "z2_nfi_price_increased/body_soap",
+    "z2_nfi_price_increased/laundry_soap",
+    "z2_nfi_price_increased/powder",
+    "z2_nfi_price_increased/toothpaste",
+    "z2_nfi_price_increased/pads"
+  )
+  
+  pmax_nfi <- function(name_nfi){
+    return(pmax(name_nfi,data.list$main[["z2_nfi_price_increased/all"]]))
+  }
+  
+  data.list$main <- data.list$main %>%
+    mutate(across(.cols = z2_cols, .fns = pmax_nfi))
+  
+  ### Additional variables are added here
+  
+  data.list$main <- data.list$main %>%
+    mutate(food_half_increased = ifelse(is.na(z1_food_price_increased), NA_real_,
+                                        ifelse(rowSums(across(z1_cols, .fns=as.numeric), na.rm=TRUE) %_>=_% length(z1_cols)/2, 
+                                               "Prices increased for AT LEAST half of food products",
+                                               "Prices increased for LESS than half of food products")),
+           nfi_half_increased = ifelse(is.na(z2_nfi_price_increased), NA_real_,
+                                       ifelse(rowSums(across(z2_cols, .fns=as.numeric), na.rm=TRUE) %_>=_% length(z2_cols)/2, 
+                                              "Prices increased for AT LEAST half of NFI products",
+                                              "Prices increased for LESS than half of NFI products")),
+           all_products_increased = ifelse(is.na(z2_nfi_price_increased) | is.na(z1_food_price_increased), NA_real_,
+                                           ifelse(rowSums(across(c(z1_cols, z2_cols), .fns=as.numeric), na.rm=TRUE) %==% (length(z2_cols) + length(z1_cols)), 
+                                                  "Prices increased for ALL products (food and NFI)",
+                                                  "Prices increased NOT for all products (food and NFI)")),
+           half_food_half_nfi_increased = ifelse(is.na(z2_nfi_price_increased) | is.na(z1_food_price_increased), NA_real_,
+                                                 ifelse(rowSums(across(z1_cols, .fns=as.numeric), na.rm=TRUE) %_>=_% length(z1_cols)/2 &
+                                                          rowSums(across(z2_cols, .fns=as.numeric), na.rm=TRUE) %_>=_% length(z2_cols)/2  , 
+                                                        "Prices increased for AT LEAST half of food AND AT LEAST half of NFI products",
+                                                        "Prices increased for LESS than half of food OR LESS than half of NFI products")),
+    )
+  
+}
